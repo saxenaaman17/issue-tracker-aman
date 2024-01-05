d73996c79be2393757a10d7d393f5902357c60b1
saxenaaman17 <saxenaaman17@gmail.com>
2024-01-05 15:02:38 +0530

    feat: add react-markdown package to view markdown text properly while rendering in Issue details page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,6 +3,7 @@ import prisma from "@/prisma/client";
 import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import React from "react";
+import Markdown from "react-markdown";
 
 interface Props {
   params: { id: string };
@@ -25,7 +26,9 @@ const IssueDetailsPage = async ({ params }: Props) => {
         <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
-      <Card>{issue.description}</Card>
+      <Card className="prose" mt="4">
+        <Markdown>{issue.description}</Markdown>
+      </Card>
     </div>
   );
 };

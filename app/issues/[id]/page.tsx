95d1701d885507fc03c090b1c54589dbdbc29834
saxenaaman17 <saxenaaman17@gmail.com>
2024-01-05 14:29:34 +0530

    feat: add issue details page

---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,29 @@
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
+import React from "react";
+
+interface Props {
+  params: { id: string };
+}
+
+const IssueDetailsPage = async ({ params }: Props) => {
+  // if we pass some string like 'abc' as id in URL bar, it'll show not found page
+  if (typeof params.id !== "number") notFound();
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue) notFound();
+
+  return (
+    <div>
+      <p>{issue.title}</p>
+      <p>{issue.description}</p>
+      <p>{issue.status}</p>
+      <p>{issue.createdAt.toDateString()}</p>
+    </div>
+  );
+};
+
+export default IssueDetailsPage;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -3,7 +3,7 @@ import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
 import prisma from "@/prisma/client";
 import IssueStatusBadge from "../components/IssueStatusBadge";
-import delay from 'delay';
+import delay from "delay";
 import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
@@ -29,7 +29,7 @@ const IssuesPage = async () => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                {issue.title}
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className="block md:hidden">
                   <IssueStatusBadge status={issue.status} />
                 </div>

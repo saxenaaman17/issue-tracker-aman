71d8351e1c2791985141491ad357967d657260ba
saxenaaman17 <saxenaaman17@gmail.com>
2024-02-03 19:19:18 +0530

    feat: add Pagination component UI, functionality is still left

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import Pagination from "./components/Pagination";
+
 const Home = () => {
-  return <div>Home Page</div>;
+  return <Pagination total={11} pageSize={10} currentPage={1} />;
 };
 
 export default Home;

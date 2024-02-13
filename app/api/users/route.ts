f4e705b8f5574c23f15865ac9541e7e381f7eedd
saxenaaman17 <saxenaaman17@gmail.com>
2024-02-13 13:31:41 +0530

    fix: remove authorization for users list api which was not showing assignee filter if not logged in

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,12 +1,13 @@
-import authOptions from "@/app/auth/authOptions";
-import { getServerSession } from "next-auth";
+// import authOptions from "@/app/auth/authOptions";
+// import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 
 // we don't need request param here but we'll keep it otherwise next.js caches the output of this api call
 export async function GET(request: NextRequest) {
-  const session = await getServerSession(authOptions);
-  if (!session) return NextResponse.json({}, { status: 401 });
+  // we want to show user filter to everyone so we can't keep users list behind authorization
+  // const session = await getServerSession(authOptions);
+  // if (!session) return NextResponse.json({}, { status: 401 });
 
   const users = await prisma.user.findMany({ orderBy: { name: "asc" } });
 
